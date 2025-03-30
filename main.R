42e6b7cd3e121669a295005257556e8f899d594c
Rick <richims026@gmail.com>
2025-03-30 00:12:53 -0500

    Initial

---
+++ main.R
@@ -0,0 +1,33 @@
+# LoadLibrary
+library(tidyverse)
+library(magrittr)
+library(ggdark)
+library(expss)
+library(lubridate)
+library(haven)
+library(pacman)
+
+# .lintr
+text <- c(
+  "linters: linters_with_defaults(",
+  "  assignment_linter(allow_pipe_assign = TRUE)",
+  "  )"
+)
+
+pathfile <- file.path(getwd(), ".lintr")
+
+writeLines(text, pathfile)
+
+options("width" = 10000)
+getOption("width")
+
+# Directorios
+file.create(".gitignore")
+file.create("test.R")
+file.create("readme.md")
+dir.create("Files")
+
+# Procesamiento
+options("width" = 10000)
+
+
